fd565b4aefc86e2d1a186655b7585c55cb82bc59
Kersarh <kers3412.git@gmail.com>
2021-09-08 15:21:03 +0300

    demo

---
+++ _Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestAES();
+        }
+
+        static void TestAES()
+        {
+            string data = "Hello AES!!!";
+            string password = "pass";
+            LibCriptoAES.API ApiAES = new();
+            var enc = ApiAES.encrypt.Encrypt(data, password);
+            Console.WriteLine(enc);
+
+            var dec = ApiAES.decrypt.Decrypt(enc, password);
+            Console.WriteLine(dec);
+
+        }
+    }
+}
