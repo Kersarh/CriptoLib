e6c779157663b3c87ea23cda7c6525afcc279813
Kersarh <kers3412.git@gmail.com>
2021-09-09 12:01:14 +0300

    RSA

--- _Demo/Program.cs
+++ _Demo/Program.cs
@@ -7,6 +7,22 @@ namespace _Demo
         static void Main(string[] args)
         {
             TestAES();
+            TestGOST();
+        }
+
+        private static void TestGOST()
+        {
+            string data = "Hello GOST!!!";
+            string password = "pass";
+
+            LibCriptoGOST.API ApiGOST = new();
+
+            var enc = ApiGOST.encrypt.Encrypt(data, password);
+            Console.WriteLine(enc);
+
+            var dec = ApiGOST.decrypt.Decrypt(enc, password);
+            Console.WriteLine(dec);
+
         }
 
         static void TestAES()
@@ -14,6 +30,7 @@ namespace _Demo
             string data = "Hello AES!!!";
             string password = "pass";
             LibCriptoAES.API ApiAES = new();
+
             var enc = ApiAES.encrypt.Encrypt(data, password);
             Console.WriteLine(enc);
 
